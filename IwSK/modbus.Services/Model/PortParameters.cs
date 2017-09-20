054f4c935413fbf18e297b0abf14dbedba936417
Mateusz Sikora <mateusz.sikora@euvic.pl>
2017-09-20 19:20:13 +0200

    poprawa nazw projektów, początek modbusa

---
+++ IwSK/modbus.Services/Model/PortParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modbus.Services.Model
+{
+    public class PortParameters
+    {
+        public int Speed { get; set; }
+
+        public string PortName { get; set; }
+
+        public int TransmissionType { get; set; }
+
+
+    }
+}
